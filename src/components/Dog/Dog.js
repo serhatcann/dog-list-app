13b5bdbcc52ac2d9dc251ca1e31820c6fa01b757
Serhat Can <serhatcan1996@gmail.com>
2021-10-02 02:58:17 +0300

    Created Dog component with css, added styled component library

--- src/components/Dog/Dog.js
+++ src/components/Dog/Dog.js
@@ -1,15 +1,31 @@
 import DogInfo from './DogInfo';
+import styled from 'styled-components';
 
-const Dog = ({ breeds, img }) => {
+const Wrapper = styled.div`
+	/* &:hover {
+		background: rgba(0, 0, 0, 0.1);
+	} */
+	margin: 10px;
+	border: solid 1px rgba(0, 0, 0, 0.2);
+	border-radius: 5px;
+	width: 400px;
+`;
+
+const Img = styled.img`
+	height: 300px;
+	justify-content: center;
+	max-width: 100%;
+`;
+
+const Dog = ({ img, id, breeds }) => {
 	return (
-		<div>
-			<img src={img} alt='' />
+		<Wrapper key={id}>
+			<Img src={img} alt={id} />
 			{breeds &&
 				breeds.map((breed) => {
-					return <div key={breed.name + breed.id}>{breed.name}</div>;
+					return <DogInfo key={breed.name + breed.id} breeds={breeds} />;
 				})}
-			<DogInfo></DogInfo>
-		</div>
+		</Wrapper>
 	);
 };
 
--- src/components/Dog/DogInfo.js
+++ src/components/Dog/DogInfo.js
@@ -1,5 +1,49 @@
-const DogInfo = () => {
-	return <div></div>;
+import styled from 'styled-components';
+
+const DogInfoTitle = styled.div`
+	font-weight: bold;
+	margin-right: 15px;
+	white-space: nowrap;
+`;
+
+const DogInfoLine = styled.div`
+	&:last-child {
+		border: none;
+	}
+	display: flex;
+	border-bottom: solid 1px rgba(0, 0, 0, 0.2);
+	padding: 7px;
+`;
+
+const DogInfo = ({ breeds: breed }) => {
+	return (
+		<>
+			<DogInfoLine>
+				<DogInfoTitle>Name:</DogInfoTitle>
+				{breed[0].name}
+			</DogInfoLine>
+			<DogInfoLine>
+				<DogInfoTitle>Breed:</DogInfoTitle>
+				{breed[0].breed_group}
+			</DogInfoLine>
+			<DogInfoLine>
+				<DogInfoTitle>Breed Details:</DogInfoTitle>
+				{breed[0].bred_for}
+			</DogInfoLine>
+			<DogInfoLine>
+				<DogInfoTitle>Temperament:</DogInfoTitle>
+				{breed[0].temperament}
+			</DogInfoLine>
+			<DogInfoLine>
+				<DogInfoTitle>Height(Imperical/Metric):</DogInfoTitle>
+				{breed[0].height.imperial}/{breed[0].height.metric}
+			</DogInfoLine>
+			<DogInfoLine>
+				<DogInfoTitle>Weight(Imperical/Metric):</DogInfoTitle>
+				{breed[0].weight.imperial}/{breed[0].weight.metric}
+			</DogInfoLine>
+		</>
+	);
 };
 
 export default DogInfo;
