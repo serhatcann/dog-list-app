c0e96032adc987685769290cd18e966be1653cab
Serhat Can <serhatcan1996@gmail.com>
2021-10-02 00:31:26 +0300

    basic Header component created.

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+	return (
+		<div>
+			<h1>DOG LIST</h1>
+			<p>When clicked below button, goodies 10 dog will be loaded.</p>
+			<button>Load Dogs</button>
+		</div>
+	);
+};
+
+export default Header;
