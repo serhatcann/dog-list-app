e2e639600312d35c0f1c8338deaf6dbe6f1a3308
Serhat Can <serhatcan1996@gmail.com>
2021-10-02 23:41:53 +0300

    Added some css to load button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,17 @@
 import { useDispatch } from 'react-redux';
 import { getDogs } from '../app/dogsSlice';
+import styled from 'styled-components';
+
+const Button = styled.button`
+	border: solid 1px rgba(0, 0, 0, 0.5);
+	border-radius: 5px;
+	padding: 10px 20px;
+	background-color: #ffffff;
+	&:hover {
+		background-color: rgba(0, 0, 0, 0.2);
+		cursor: pointer;
+	}
+`;
 
 const Header = () => {
 	const dispatch = useDispatch();
@@ -12,7 +24,7 @@ const Header = () => {
 		<div>
 			<h1>DOG LIST</h1>
 			<p>When clicked below button, goodies 10 dog will be loaded.</p>
-			<button onClick={loadDogs}>Load Dogs</button>
+			<Button onClick={loadDogs}>Load Dogs 🐶</Button>
 		</div>
 	);
 };
