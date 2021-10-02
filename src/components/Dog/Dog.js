3524f3e4b699bf98ce507e5391277e07c9656431
Serhat Can <serhatcan1996@gmail.com>
2021-10-02 17:58:37 +0300

    Added some responsiveness for generic resolutions

--- src/components/Dog/Dog.js
+++ src/components/Dog/Dog.js
@@ -2,19 +2,29 @@ import DogInfo from './DogInfo';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-	/* &:hover {
-		background: rgba(0, 0, 0, 0.1);
-	} */
 	margin: 10px;
 	padding: 10px;
 	border: solid 1px rgba(0, 0, 0, 0.2);
 	border-radius: 5px;
 	width: 500px;
+	@media (min-width: 770px) and (max-width: 1030px) {
+		width: 100%;
+	}
+
+	@media (max-width: 768px) {
+		width: 100%;
+	}
 `;
 
 const Img = styled.img`
 	height: 300px;
 	max-width: 100%;
+	@media (min-width: 770px) and (max-width: 1030px) {
+		float: left;
+		margin-right: 10px;
+		height: 100%;
+		width: 400px;
+	}
 `;
 
 const Dog = ({ img, id, breeds }) => {
--- src/components/Dog/DogInfo.js
+++ src/components/Dog/DogInfo.js
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 
 const DogInfoTitle = styled.div`
 	font-weight: bold;
-	white-space: nowrap;
 	text-align: left;
+	word-wrap: break-word;
 	width: 50%;
 `;
 const DogInfoValue = styled.div`
 	text-align: left;
 	width: 50%;
+	word-wrap: break-word;
 `;
 
 const DogInfoLine = styled.div`
@@ -17,38 +18,38 @@ const DogInfoLine = styled.div`
 	}
 	display: flex;
 	border-bottom: solid 1px rgba(0, 0, 0, 0.2);
-	padding: 7px;
+	padding: 3px;
 `;
 
 const DogInfo = ({ breeds: breed }) => {
 	return (
 		<>
 			<DogInfoLine>
-				<DogInfoTitle>Name:</DogInfoTitle>
+				<DogInfoTitle>Name</DogInfoTitle>
 				<DogInfoValue>{breed[0].name}</DogInfoValue>
 			</DogInfoLine>
 			<DogInfoLine>
-				<DogInfoTitle>Breed:</DogInfoTitle>
+				<DogInfoTitle>Breed</DogInfoTitle>
 				<DogInfoValue>{breed[0].breed_group}</DogInfoValue>
 			</DogInfoLine>
 			<DogInfoLine>
-				<DogInfoTitle>Breed Details:</DogInfoTitle>
+				<DogInfoTitle>Breed Details</DogInfoTitle>
 				<DogInfoValue>{breed[0].bred_for}</DogInfoValue>
 			</DogInfoLine>
 			<DogInfoLine>
-				<DogInfoTitle>Temperament:</DogInfoTitle>
+				<DogInfoTitle>Temperament</DogInfoTitle>
 				<DogInfoValue>{breed[0].temperament}</DogInfoValue>
 			</DogInfoLine>
 			<DogInfoLine>
-				<DogInfoTitle>Height(Imperical/Metric):</DogInfoTitle>
+				<DogInfoTitle>Height(Imperical / Metric)</DogInfoTitle>
 				<DogInfoValue>
-					{breed[0].height.imperial}/{breed[0].height.metric}
+					{breed[0].height.imperial} / {breed[0].height.metric}
 				</DogInfoValue>
 			</DogInfoLine>
 			<DogInfoLine>
-				<DogInfoTitle>Weight(Imperical/Metric):</DogInfoTitle>
+				<DogInfoTitle>Weight(Imperical / Metric)</DogInfoTitle>
 				<DogInfoValue>
-					{breed[0].weight.imperial}/{breed[0].weight.metric}
+					{breed[0].weight.imperial} / {breed[0].weight.metric}
 				</DogInfoValue>
 			</DogInfoLine>
 		</>
