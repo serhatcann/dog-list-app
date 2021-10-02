062e3cebbff585191f0048dc3c8602a91f399c17
Serhat Can <serhatcan1996@gmail.com>
2021-10-02 03:34:20 +0300

    Fetched dogs from api with button click

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
+import { useDispatch } from 'react-redux';
+import { getDogs } from '../app/dogsSlice';
+
 const Header = () => {
+	const dispatch = useDispatch();
+
+	const loadDogs = (params) => {
+		dispatch(getDogs());
+	};
+
 	return (
 		<div>
 			<h1>DOG LIST</h1>
 			<p>When clicked below button, goodies 10 dog will be loaded.</p>
-			<button>Load Dogs</button>
+			<button onClick={loadDogs}>Load Dogs</button>
 		</div>
 	);
 };
