4076c7c1bde171992c60415b9551885d41594869
Serhat Can <serhatcan1996@gmail.com>
2021-10-03 00:06:44 +0300

    Created utility method for voting

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,11 +1,31 @@
-const URL = 'https://api.thedogapi.com/v1/images/search?';
+const getURL = 'https://api.thedogapi.com/v1/images/search?';
+const voteURL = 'https://api.thedogapi.com/v1/votes';
 
-export const fetchDogsFromApi = async ({ apiURL = URL, limit = 10, page = 10, order = 'Desc' }) => {
+export const fetchDogsFromApi = async ({ apiURL = getURL, limit = 10, page = 10, order = 'Desc' }) => {
 	try {
 		const response = await fetch(`${apiURL}limit=${limit}&page=${page}&order=${order}}`, {
 			method: 'GET',
 			headers: {
 				'X-Api-Key': process.env.REACT_APP_DOG_API_KEY,
+			},
+		});
+		return response.json();
+	} catch (error) {
+		return error;
+	}
+};
+
+export const voteDogsApi = async ({ apiURL = voteURL, imageId, vote }) => {
+	try {
+		const response = await fetch(apiURL, {
+			method: 'POST',
+			headers: {
+				'X-Api-Key': process.env.REACT_APP_DOG_API_KEY,
+				'Content-Type': 'application/json',
+			},
+			body: {
+				image_id: imageId,
+				value: vote,
 			},
 		});
 		return response.json();
